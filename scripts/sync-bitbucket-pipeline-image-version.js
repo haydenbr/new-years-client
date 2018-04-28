d651453a5b9cd01e73dfb42cbb810195a4874018
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-04-28 02:10:27 -0400

    chore(build): update message that's shown after updating bitbucket pipelines

--- scripts/sync-bitbucket-pipeline-image-version.js
+++ scripts/sync-bitbucket-pipeline-image-version.js
@@ -3,14 +3,12 @@ const util = require('./script-utilities');
 
 module.exports = () => {
 	let pipelinesFilepath = path.resolve(__dirname, '..', 'bitbucket-pipelines.yml');
-	let version = util.getCurrentVersion();
+	let image = util.getCurrentDockerImage();
 
 	return util
 		.readFile(pipelinesFilepath)
 		.then(pipeline => util.convertYamlToJson(pipeline))
 		.then(pipelineObject => {
-			let image = util.getCurrentDockerImage();
-
 			if (pipelineObject.image.name) {
 				pipelineObject.image.name = image;
 			} else {
@@ -20,5 +18,5 @@ module.exports = () => {
 			return util.convertJsonToYaml(pipelineObject);
 		})
 		.then(updatedPipeline => util.writeFile(pipelinesFilepath, updatedPipeline))
-		.then(() => console.log(`updated bitbucket pipelines image version to ${version}`));
+		.then(() => console.log(`updated bitbucket pipelines image to ${image}`));
 };
